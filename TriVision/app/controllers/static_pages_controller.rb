3406dbe4560ff6d809abc0ee98f76442552ae12b
Vimal Bhalodia <vimal@alum.mit.edu>
2015-08-02 10:07:37 -0700

    added in templating

--- TriVision/app/controllers/static_pages_controller.rb
+++ TriVision/app/controllers/static_pages_controller.rb
@@ -10,7 +10,28 @@ class StaticPagesController < ApplicationController
     master_file = data_root+"/urbnearth.master.json"
     # puts "master_file", master_file
     @master_json = JSON.parse(File.read(master_file))
-    puts "master_json", @master_json
+    #puts "master_json", @master_json
+    
+    @communities = []
+    @master_json['advertising']['communities'].each do |profile_id|
+      profile_json = JSON.parse(File.read(data_root+"/"+profile_id+".info.json"))
+      profile_json['id'] = profile_json['id'].to_s
+      @communities.push(profile_json)
+    end
+    
+    @media = []
+    @master_json['advertising']['media'].each do |profile_id|
+      profile_json = JSON.parse(File.read(data_root+"/"+profile_id+".info.json"))
+      profile_json['id'] = profile_json['id'].to_s
+      @media.push(profile_json)
+    end
+
+    @celebrities = []
+    @master_json['advertising']['celebrities'].each do |profile_id|
+      profile_json = JSON.parse(File.read(data_root+"/"+profile_id+".info.json"))
+      profile_json['id'] = profile_json['id'].to_s
+      @celebrities.push(profile_json)
+    end
     
   end
 
